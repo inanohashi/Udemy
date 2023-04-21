d6ee198a334e8c8b196c44529cb9b6cfe4ea0cbf
inanohashi <yoshii.nanoko@hrbrain.co.jp>
2023-04-21 18:07:38 +0900

    3の倍数で顔文字が出るようにした

--- react/react_app/src/App.jsx
+++ react/react_app/src/App.jsx
@@ -1,29 +1,38 @@
-import React, { useState } from "react";
-import ColorfulMessage from './components/ColorfulMessage';
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
-const App = () => {
-    const [num, setNum] = useState(0);
-    const [faceShowFlag, setFaceShowFlag] = useState(true);
+export const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
 
-    const onClickCountUp = () => {
-        setNum(num + 1)
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+useEffect(() => {
+  if (num > 0) {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
     }
-    const onClickSwitchShowFlag = () => {
-        setFaceShowFlag(!faceShowFlag)
-    }
+  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [num]);
 
-    return (
-        <>
-            <h1 style={{ color: 'red' }}>こんにちは!</h1>
-            <ColorfulMessage color='blue'>お元気ですか？</ColorfulMessage>
-            <ColorfulMessage color='pink'>元気です!</ColorfulMessage>
-            <br />
-            <button onClick={onClickSwitchShowFlag}>on/off</button>
-            <button onClick={onClickCountUp}>カウントアップ</button>
-            <p>{num}</p>
-            {faceShowFlag && <p>(^o^)</p>}
-        </>
-    );
-}
-
-export default App;
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは!</h1>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です!</ColorfulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(^o^)</p>}
+    </>
+  );
+};
